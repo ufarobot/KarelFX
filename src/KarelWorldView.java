c652ba3931ecd42b0c62acd1186b23a2e2055f95
ufarobot <ufarobot@gmail.com>
2017-11-03 13:45:09 +0300

    - added gson support - code refactored

--- src/KarelWorldView.java
+++
@@ -1,9 +0,0 @@
-import javafx.scene.Parent;
-
-/**
- * Created by Airat Ishimbaev on 24.08.2017.
- */
-public class KarelWorldView extends Parent {
-    KarelWorld world;
-
-}
--- src/WDirection.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Airat Ishimbaev on 24.08.2017.
- */
-public enum WDirection {
-    NORTH,
-    WEST,
-    EAST,
-    SOUTH
-}
